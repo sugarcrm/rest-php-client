e63aa38678ff98153a30a4cd8f168ab8d47ec6f4
Anthony <crazytonyi@gmail.com>
2023-08-04 11:29:30 -0500

    SUPP-3336: test content

--- tests/Endpoint/AbstractSugarBeanEndpointTest.php
+++ tests/Endpoint/AbstractSugarBeanEndpointTest.php
@@ -273,18 +273,30 @@ class AbstractSugarBeanEndpointTest extends \PHPUnit\Framework\TestCase
     public function testAuditLog()
     {
         $Bean = new Module();
-        self::$client->mockResponses->append(new Response(200));
+
+        $auditResponse = [
+            'records' => [
+                [
+                    'id' => '12345',
+                    'parent_id' => 'some_parent_id',
+                ]
+            ],
+        ];
+        self::$client->mockResponses->append(new Response(200, [], json_encode($auditResponse)));
+
         $Bean->setClient(self::$client);
         self::$client->setVersion("10");
         $Bean->setUrlArgs(['Foo', 'bar']);
         $Audit = $Bean->auditLog(100);
+        $auditArray = $Audit->toArray();
+        $firstAuditRecord = reset($auditArray);
         $this->assertInstanceOf('Sugarcrm\\REST\\Endpoint\\ModuleAudit', $Audit);
+        $this->assertEquals($auditResponse['records'][0],$firstAuditRecord);
         $this->assertEquals('/rest/v11_11/Foo/bar/audit', self::$client->mockResponses->getLastRequest()->getUri()->getPath());
         parse_str(self::$client->mockResponses->getLastRequest()->getUri()->getQuery(), $query);
         $this->assertEquals(100, $query['max_num']);
         $this->assertEquals('GET', self::$client->mockResponses->getLastRequest()->getMethod());
     }
-
 
     /**
      * @covers ::filterRelated
